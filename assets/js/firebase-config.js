2f1fabec0048d855fb5f88456dc293827b296769
Navkar Apotek LLP <navkarapotek@gmail.com>
2026-07-14 15:42:12 +0530

    Add files via upload

---
+++ assets/js/firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyChYHB0CZF2ZfBOBzWsozP9Fz533E0RJ-4",
+  authDomain: "navkartokensystem.firebaseapp.com",
+  projectId: "navkartokensystem",
+  storageBucket: "navkartokensystem.firebasestorage.app",
+  messagingSenderId: "1038142281136",
+  appId: "1:1038142281136:android:591aca47e4b6865ec97275"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
