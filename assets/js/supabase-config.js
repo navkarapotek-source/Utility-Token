64470e3386546898a447b34d7a4f23266fcb25df
Navkar Apotek LLP <navkarapotek@gmail.com>
2026-07-22 16:35:19 +0530

    Add files via upload

---
+++ assets/js/supabase-config.js
@@ -0,0 +1,3 @@
+const SUPABASE_URL = 'https://lvrkknhdpokzdabaeinl.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_ez0WHZzeoJazbbQIqwHoBw_dqmgvyyb';
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
